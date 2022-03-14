b04f5305a7e2cd7143f238bbf3623be54899d30c
Akshar-Patel1 <92375661+Akshar-Patel1@users.noreply.github.com>
2022-03-14 16:35:03 +0000

    Unit 2: Lesson 1 - Strings and Class Types

--- Main.java
+++ Main.java
@@ -5,17 +5,7 @@ public class Main {
     public static void main(String[] args) { 
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Enter the Feet: ");
-        int feet1 = scan.nextInt();
-        System.out.print("Enter the Inches: ");
-        int inches1 = scan.nextInt();
-        System.out.print("Enter the Feet: ");
-        int feet2 = scan.nextInt();
-        System.out.print("Enter the Inches: ");
-        int inches2 = scan.nextInt();
-        scan.close();
-        int ft = (int) (feet1 + feet2) + ((inches1 + inches2) / 12);
-        int inch = (int) (inches1 + inches2) % 12;
-        System.out.println("Feet: " + ft + " Inches: " + inch);
+        int d1h = 54;
+        System.out.print(d1h);
     }
 }
