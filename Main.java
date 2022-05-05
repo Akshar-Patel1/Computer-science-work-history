67b4f237c560d0b9db564482c74c697256b756a4
Akshar-Patel1 <92375661+Akshar-Patel1@users.noreply.github.com>
2022-05-05 18:01:29 +0000

    project stif

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
-public class Main { 
+public class Main {
 
-    public static void main(String[] args) { 
-        Scanner scan = new Scanner(System.in);
+  public static void main (String args[]) {
+    Scanner scan = new Scanner(System.in);
+    int d = scan.nextInt();
+    double e = ((d * 1.0 * 9/5) + 32);
+    System.out.println(e);
 
-        int d1h = 54;
-        System.out.print(d1h);
-    }
+  }
+    
+
 }
