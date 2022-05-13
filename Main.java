ad0dbf6514db0f3f0daa8cb056b4a3c00c138def
Akshar-Patel1 <92375661+Akshar-Patel1@users.noreply.github.com>
2022-05-13 15:53:26 +0000

    unit 3 lesson 2

--- Main.java
+++ Main.java
@@ -4,9 +4,9 @@ public class Main {
 
   public static void main (String args[]) {
     Scanner scan = new Scanner(System.in);
-    int d = scan.nextInt();
-    double e = ((d * 1.0 * 9/5) + 32);
-    System.out.println(e);
+    int p = scan.nextInt();
+    int y = (int) Math.random() * 3 + 1;
+    
 
   }
     
