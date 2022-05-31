2f396b994ad7b0d758cd9f87528c7771a7f36ff3
Akshar-Patel1 <92375661+Akshar-Patel1@users.noreply.github.com>
2022-05-31 17:09:48 +0000

    3.6 / 3.7 / rts

--- Main.java
+++ Main.java
@@ -3,42 +3,21 @@ import java.util.Scanner;
 
 public class Main {
   
-  public static String playgame(String userinput, String computerinput) {
-      String whowon = "hello";
-      if (userinput.equals(computerinput)) {
-        whowon = "Tie!";
-      } else if (userinput.equals("rock") && computerinput.equals("paper") || userinput.equals("paper") && computerinput.equals("scissors") || userinput.equals("scissors") && computerinput.equals("rock")) {
-        whowon = "Computer Won!";
-      } else if (computerinput.equals("rock") && userinput.equals("paper") || computerinput.equals("paper") && userinput.equals("scissors") || computerinput.equals("scissors") && userinput.equals("rock")) {
-          whowon = "User Won!";
-      } else {
-        whowon = "error";
-      };
-      return(whowon);
-    }
 
-
-
-  
   public static void main (String args[]) {
     Scanner scan = new Scanner(System.in);
-    System.out.println("Please choose either: \n\"rock\"\n\"paper\"\n\"scissors\"");
-    String p = scan.nextLine().toLowerCase();
-    double ran = Math.random();
-    int answer = (int) (ran * 3) + 1;
-    String computerguess;
-    if (answer == 1) {
-      computerguess = "rock";
-    } else if (answer == 2) {
-      computerguess = "paper";
-    } else {
-      computerguess = "scissors";
+    System.out.println("What is your guess?\n1 - Rock\n2 - Paper\n3 - Scissors");
+    int personguess = scan.nextInt();
+    int computerguess = (int) (Math.random() * 3 + 1);
+    if ((personguess == 1 && computerguess == 2) || (personguess == 2 && computerguess == 3) || (personguess == 3 && computerguess == 1)) {
+      System.out.println("Computer Won!");
     }
-    scan.close();
-    
-    System.out.println("User Guess: " + p + "\nComputer Guess: " + computerguess + "\n" + playgame(p, computerguess));
-
+    if ((personguess == 1 && computerguess == 3) || (personguess == 2 && computerguess == 1) || (personguess == 3 && computerguess == 2)) {
+      System.out.println("You Won!");
+    }
+    if (personguess == computerguess) {
+      System.out.println("Tie!");
+    }
+    }
   }
     
-
-}
