0836cff3b86388e31a747aac988e72f80cf718f1
Akshar-Patel1 <92375661+Akshar-Patel1@users.noreply.github.com>
2022-06-10 17:07:53 +0000

    4.1/4.15

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-// Rock paper Sicssors
+ // Rock paper Sicssors
 import java.util.Scanner;
 
 public class Main {
@@ -9,12 +9,18 @@ public class Main {
     System.out.println("What is your guess?\n1 - Rock\n2 - Paper\n3 - Scissors");
     int personguess = scan.nextInt();
     int computerguess = (int) (Math.random() * 3 + 1);
+
+    //lose
     if ((personguess == 1 && computerguess == 2) || (personguess == 2 && computerguess == 3) || (personguess == 3 && computerguess == 1)) {
       System.out.println("Computer Won!");
     }
+
+    //win
     if ((personguess == 1 && computerguess == 3) || (personguess == 2 && computerguess == 1) || (personguess == 3 && computerguess == 2)) {
       System.out.println("You Won!");
     }
+
+    //tie
     if (personguess == computerguess) {
       System.out.println("Tie!");
     }
